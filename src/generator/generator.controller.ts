161b6adf2d19e10a328622fe49b0006c928f4376
Dmytro Uvin <dimavfox@gmail.com>
2023-08-11 13:38:56 +0300

    Add test method

--- src/generator/generator.controller.ts
+++ src/generator/generator.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { GeneratorService } from './generator.service';
 import { MainGeneratorDto } from '@generator/dto';
 import { Response } from 'express';
+import puppeteer from 'puppeteer';
 
 @Controller('generator')
 export class GeneratorController {
@@ -38,5 +39,15 @@ export class GeneratorController {
   @Get('test-queue-scaling')
   public async test() {
     await this.generatorService.test();
+  }
+
+  @Get('puppeteer-test')
+  public async testPuppeteer() {
+    const browser = await puppeteer.launch({
+      headless: 'new',
+    });
+    const page = await browser.newPage();
+    const view = await page.goto('https://google.com');
+    await browser.close();
   }
 }
