b2dbd2c64c7ae69219bc3e67531b663d06ea31f2
Dmytro Uvin <dimavfox@gmail.com>
2023-10-19 16:24:57 +0300

    Fix docs

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('test')
 export class TestController {
   public constructor(@InjectQueue('test-job') private testJobQueue: Queue) {}
 
+  @ApiExcludeEndpoint()
   @Get('api')
   public async testApi() {
     console.log('test api');
