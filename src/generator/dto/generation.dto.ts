ec41a8c02ed486794ee6646cf6eb9e076d3f79e0
Dmytro Uvin <dimavfox@gmail.com>
2023-10-25 15:57:06 +0300

    Fix DI

--- src/generator/dto/generation.dto.ts
+++ src/generator/dto/generation.dto.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GenerationDto {
+  @ApiProperty({
+    type: String,
+  })
   id: string;
+  @ApiProperty({
+    type: String,
+  })
   prompt: string;
+  @ApiProperty({
+    type: Number,
+  })
   progressInPercents: number;
+  @ApiProperty({
+    type: String,
+  })
   createdAt: string;
+  @ApiProperty({
+    type: String,
+  })
   userId: string;
 }
